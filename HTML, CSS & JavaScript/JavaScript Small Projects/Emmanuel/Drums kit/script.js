d9537788a3391f296949735a339d43eb09f46682
Emmanuel Ngongo <129612476+EmmanuelNgongo@users.noreply.github.com>
2023-06-28 20:38:58 +0200

    js

--- HTML, CSS & JavaScript/JavaScript Small Projects/Emmanuel/Drums kit/script.js
+++ HTML, CSS & JavaScript/JavaScript Small Projects/Emmanuel/Drums kit/script.js
@@ -3,3 +3,12 @@
 window.addEventListener('keydown', function(e){
     console.log(e.keycode);
 })
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    if (!audio) return;
+
+    key.classList.add('playing');
+    audio.currentTime = 0;
+    audio.play();
+  }
